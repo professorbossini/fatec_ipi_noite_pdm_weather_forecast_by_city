7e48c205230d16c4279d3474b5c4b04b06171c61
Rodrigo Bossini <professorbossini@gmail.com>
2019-09-20 22:16:46 -0300

    Fizemos o consumo do WS usando Threads simples e mostramos o JSON em um Toast.

--- app/src/main/java/br/com/bossini/fatec_ipi_noite_pdm_weather_forecast_by_city/MainActivity.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_noite_pdm_weather_forecast_by_city/MainActivity.java
@@ -8,13 +8,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -40,34 +49,79 @@ public class MainActivity extends AppCompatActivity {
                         weatherList
                 );
         weatherListView.setAdapter(adapter);
-
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener((v) -> {
+            String cidade =
+                    locationEditText.getEditableText().toString();
+            String end =
+                    getString(
+                        R.string.web_service_url,
+                        cidade,
+                        getString(
+                                R.string.api_key
+                        )
+                    );
 
+            new Thread (
+                    () -> {
+                        try{
+                            URL url = new URL (end);
+                            HttpURLConnection conn =
+                                    (HttpURLConnection) url.openConnection();
+                            InputStream is =
+                                    conn.getInputStream();
+                            InputStreamReader isr =
+                                    new InputStreamReader(is);
+                            BufferedReader reader =
+                                    new BufferedReader(isr);
+                            String linha = null;
+                            StringBuilder resultado =
+                                    new StringBuilder("");
+                            while ((linha = reader.readLine()) != null){
+                                resultado.append(linha);
+                            }
+                            reader.close();
+                            conn.disconnect();
+                            runOnUiThread( () -> {
+                                Toast.makeText(
+                                        this,
+                                        resultado.toString(),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            });
+                        }
+                        catch (MalformedURLException e){
+                            e.printStackTrace();
+                            runOnUiThread(() ->{
+                                Toast.makeText(
+                                        this,
+                                        getString(
+                                                R.string.invalid_url
+                                        ),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            });
+
+                        }
+                        catch (IOException e){
+                            e.printStackTrace();
+                            runOnUiThread(() ->{
+                                Toast.makeText(
+                                        this,
+                                        getString(
+                                                R.string.connect_error
+                                        ),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            });
+                        }
+                    }
+            ).start();
+
+
+            //Log.i("meulog", end);
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
